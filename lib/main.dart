d732def199dd4bbc3f1e210c9f9a1fb965ab8fab
tayla-tineu <dantas.tayla@gmaill.com>
2020-05-16 19:00:56 -0300

    aula 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:flame/time.dart';
 
 void main() {
   runApp(GameWidget());
@@ -7,16 +8,39 @@ void main() {
 
 class SpaceShooterGame extends Game {
   GameObject player;
+  List<GameObject> enemies =[];
+  static const enemy_speed = 400;
+  Timer enemyCreator;
+
   SpaceShooterGame()
   {
     player = GameObject()..position = Rect.fromLTWH(100, 100, 50, 50);
+    enemyCreator = Timer(1.0,repeat:true,callback:() {
+      enemies.add(
+          GameObject()
+            ..position = Rect.fromLTWH(0, 0, 50, 50)
+      );
+    });
+    enemyCreator.start();
+
   }
   void onPlayerMove(Offset delta)
   {
     player.position = player.position.translate(delta.dx,delta.dy);
   }
-  @override  void update(double dt) {  }
-  @override  void render(Canvas canvas){  player.render(canvas);  }
+  @override  void update(double dt) {
+
+    enemies.forEach((enemy) {
+      enemy.position = enemy.position.translate(0, enemy_speed * dt);
+    });
+    enemyCreator.update(dt);
+
+  }
+  @override  void render(Canvas canvas){
+    enemies.forEach((enemy) {
+       enemy.render(canvas);
+    });
+    player.render(canvas);  }
   }
 class GameWidget extends StatelessWidget {
   @override  Widget build(BuildContext context){
